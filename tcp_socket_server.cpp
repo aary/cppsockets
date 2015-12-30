72ea116fdf1ee2f48c4216b026242890e4d75101
Aaryaman Sagar <aary@Aaryamans-MacBook-Pro.local>
2015-12-30 14:53:55 +0530

    Changed README.md

--- tcp_socket_server.cpp
+++ tcp_socket_server.cpp
@@ -2,20 +2,28 @@
 #include <array>
 #include <stdexcept>
 #include <sstream>
-#include "SocketUtilities.h"
+#include "SocketUtilities/SocketUtilities.h"
 using namespace std;
 
 string response = 
 "HTTP/1.1 200 OK\n\n"
 "Hello, World!\n";
 
-int main() {
+int main(int argc, char** argv) {
+    
+    // Error check command line arguments
+    if (argc != 2) {
+        cerr << "Usage: " << argv[0] << " <port_number>" << endl;
+        exit(1);
+    }
+
     using SocketRAII = SocketUtilities::SocketRAII;
 
     // create socket on which the server will listen
     SocketRAII sockfd = SocketUtilities::create_server_socket("8000");
 
-    cout << "Serving on port " << "8000" << endl;
+    // Print serving prompt
+    cout << " * Serving on port " << argv[1] << " (Press CTRL+C to quit)" << endl;
 
     array<char, 1024> buffer;
     while (true) {  // main accept() loop
